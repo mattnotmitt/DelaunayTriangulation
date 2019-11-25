7a0d3baac95e23b3ae2b92694c06dc718d014b7a
Matt <matt@artemisbot.uk>
2019-11-25 09:44:16 +0000

    File loading and saving support

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -4,6 +4,9 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
 #include "Triangle.hpp"
 
 std::istream &operator>>(std::istream &is, Triangle &triangle) {
@@ -14,10 +17,10 @@ std::istream &operator>>(std::istream &is, Triangle &triangle) {
         vertices.push_back((int) std::strtol(vertexIndex.c_str(), NULL, 10));
     }
     triangle.setVertices(vertices);
-    std::vector<float> attributes;
+    std::vector<double> attributes;
     std::string next;
     while (is >> next) {
-        attributes.push_back((float) std::strtod(next.c_str(), NULL));
+        attributes.push_back(std::strtod(next.c_str(), NULL));
     }
     triangle.setAttributes(attributes);
     return is;
@@ -39,11 +42,21 @@ void Triangle::setVertices(const std::vector<int> &v) {
     Triangle::vertices = v;
 }
 
-const std::vector<float> &Triangle::getAttributes() const {
+const std::vector<double> &Triangle::getAttributes() const {
     return attributes;
 }
 
-void Triangle::setAttributes(const std::vector<float> &a) {
+void Triangle::setAttributes(const std::vector<double> &a) {
     Triangle::attributes = a;
+}
+
+std::ofstream &operator<<(std::ofstream &os, Triangle &triangle) {
+    std::stringstream ss;
+    ss << std::setprecision(6) << triangle.index << " " << triangle.vertices[0] << " " << triangle.vertices[1] << " " << triangle.vertices[2];
+    for (int i = 0; i < triangle.attributes.size(); i++) {
+        ss << " " << std::setprecision(12) << triangle.attributes[i];
+    }
+    os << ss.str().c_str() << "\n";
+    return os;
 }
 
--- tests/mesh.cpp
+++ tests/mesh.cpp
@@ -11,10 +11,20 @@ TEST_CASE("Mesh is constructed", "[mesh]") {
 }
 
 TEST_CASE("Mesh is populated from file", "[mesh]") {
-    Mesh defMesh = Mesh();
+    Mesh defMesh;
     defMesh.loadFile(std::string("../tests/data/triangulation_files/triangulation#1.tri"));
     REQUIRE(defMesh.getVertices().size() == 1467);
-    REQUIRE(defMesh.getVertices().at(0).getAttributes().size() == 0);
+    REQUIRE(defMesh.getVertices().at(0).getAttributes().empty());
     REQUIRE(defMesh.getTriangles().size() == 2620);
     REQUIRE(defMesh.getTriangles().at(0).getAttributes().size() == 17);
+}
+
+TEST_CASE("Triangulation file can be populated from mesh", "[mesh]") {
+    Mesh defMesh;
+    defMesh.loadFile(std::string("../tests/data/triangulation_files/triangulation#4.tri"));
+    std::ofstream outfile("../build/triangulation#4_copied.tri", std::ios::binary);
+    if (!outfile.is_open()) {
+        throw std::runtime_error("Could not open output file.");
+    }
+    outfile << defMesh;
 }
